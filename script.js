e88b62508f34cc460be9df5ad7008dea427037d3
tarikjamil <tarikjamill@gmail.com>
2023-10-05 15:12:17 +0100

    edits

--- script.js
+++ script.js
@@ -18,28 +18,24 @@ function pageLoad() {
 }
 pageLoad();
 
-$(".navbar--dropdown-trigger").on("click", function () {
-  var dropdown = $(this).closest(".navbar--dropdown");
-  var bg = $(".menu--dropdown-content-bg");
+$(document).ready(function () {
+  $(".navbar--dropdown-trigger").on("click", function () {
+    if ($(window).width() >= 998) {
+      // Ensure we're above 998px window width
+      // For all .navbar--dropdown-content
+      gsap.to(".navbar--dropdown-content", { height: "auto" });
 
-  if (dropdown.hasClass("is--active")) {
-    // Animate height to 0
-    gsap.to(dropdown, {
-      height: 0,
-      onComplete: function () {
-        dropdown.removeClass("is--active");
-      },
-    });
-    bg.removeClass("is--active"); // You can also animate this with GSAP if needed
-  } else {
-    // Animate height to its "auto" value
-    gsap.set(dropdown, { height: "auto" });
-    gsap.from(dropdown, {
-      height: 0,
-      onComplete: function () {
-        dropdown.addClass("is--active");
-      },
-    });
-    bg.addClass("is--active"); // Again, animate with GSAP if necessary
-  }
+      // For sibling .navbar--dropdown-content of the clicked trigger
+      var siblingDropdownContent = $(this).siblings(
+        ".navbar--dropdown-content"
+      );
+      gsap.to(siblingDropdownContent.find("a"), { opacity: 1 });
+
+      // For .menu--dropdown-content-bg
+      $(".menu--dropdown-content-bg").css("display", "block"); // Alternatively, you can animate the opacity with GSAP from 0 to 1
+
+      // For .div-block-2
+      gsap.to(".div-block-2", { height: "auto" });
+    }
+  });
 });
