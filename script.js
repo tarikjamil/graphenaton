2ab66e346eba16c27592c78aa1e562b61b463579
tarikjamil <tarikjamill@gmail.com>
2023-10-05 15:18:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,16 +28,30 @@ $(document).ready(function () {
 
       if (siblingDropdownContent.height() === 0) {
         // Opening actions
-        gsap.to(".navbar--dropdown-content", { height: "auto" });
-        gsap.to(siblingDropdownContent.find("a"), { opacity: 1 });
+        gsap.to(".navbar--dropdown-content", {
+          height: "auto",
+          ease: "power5.out",
+        });
+        gsap.to(siblingDropdownContent.find("a"), {
+          opacity: 1,
+          ease: "power5.out",
+        });
         $(".menu--dropdown-content-bg").css("display", "block");
-        gsap.to(".div-block-2", { height: "auto" });
+        gsap.to(".div-block-2", { height: "auto", ease: "power5.out" });
       } else {
         // Closing actions
-        gsap.to(".navbar--dropdown-content", { height: 0 });
-        gsap.to(siblingDropdownContent.find("a"), { opacity: 0 });
-        $(".menu--dropdown-content-bg").css("display", "none"); // Alternatively, you can animate the opacity with GSAP from 1 to 0
-        gsap.to(".div-block-2", { height: 0 });
+        gsap.to(".navbar--dropdown-content", { height: 0, ease: "power5.out" });
+        gsap.to(siblingDropdownContent.find("a"), {
+          opacity: 0,
+          ease: "power5.out",
+        });
+        gsap.to(".div-block-2", {
+          height: 0,
+          ease: "power5.out",
+          onComplete: function () {
+            $(".menu--dropdown-content-bg").css("display", "none");
+          },
+        });
       }
     }
   });
