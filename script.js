d7e70493560970525e9cb23fd82c359be56c7254
tarikjamil <tarikjamill@gmail.com>
2023-10-05 15:29:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,25 +37,37 @@ $(document).ready(function () {
         // Opening actions
         gsap.to(".navbar--dropdown-content", {
           height: "auto",
+          duration: 0.3,
           ease: "power5.out",
         });
         gsap.to(siblingDropdownContent.find("a"), {
           opacity: 1,
+          duration: 0.3,
           ease: "power5.out",
         });
         $(this).find(".navbar--dropdown-activestate").css("opacity", 1);
         $(".menu--dropdown-content-bg").css("display", "block");
-        gsap.to(".div-block-2", { height: "auto", ease: "power5.out" });
+        gsap.to(".div-block-2", {
+          height: "auto",
+          duration: 0.3,
+          ease: "power5.out",
+        });
       } else {
         // Closing actions
-        gsap.to(".navbar--dropdown-content", { height: 0, ease: "power5.out" });
+        gsap.to(".navbar--dropdown-content", {
+          height: 0,
+          duration: 0.3,
+          ease: "power5.out",
+        });
         gsap.to(siblingDropdownContent.find("a"), {
           opacity: 0.5,
+          duration: 0.3,
           ease: "power5.out",
         });
         $(this).find(".navbar--dropdown-activestate").css("opacity", 0);
         gsap.to(".div-block-2", {
           height: 0,
+          duration: 0.3,
           ease: "power5.out",
           onComplete: function () {
             $(".menu--dropdown-content-bg").css("display", "none");
