1b715ad5915faa67f9825417b5e661d1783a3f1d
tarikjamil <tarikjamill@gmail.com>
2023-10-05 15:25:40 +0100

    edits

--- script.js
+++ script.js
@@ -21,10 +21,17 @@ pageLoad();
 $(document).ready(function () {
   $(".navbar--dropdown-trigger").on("click", function () {
     if ($(window).width() >= 998) {
-      // Ensure we're above 998px window width
       var siblingDropdownContent = $(this).siblings(
         ".navbar--dropdown-content"
       );
+
+      // Reset all other dropdown triggers and contents
+      $(".navbar--dropdown-content a")
+        .not(siblingDropdownContent.find("a"))
+        .css("opacity", 0.5);
+      $(".navbar--dropdown-activestate")
+        .not($(this).find(".navbar--dropdown-activestate"))
+        .css("opacity", 0);
 
       if (siblingDropdownContent.height() === 0) {
         // Opening actions
@@ -36,15 +43,17 @@ $(document).ready(function () {
           opacity: 1,
           ease: "power5.out",
         });
+        $(this).find(".navbar--dropdown-activestate").css("opacity", 1);
         $(".menu--dropdown-content-bg").css("display", "block");
         gsap.to(".div-block-2", { height: "auto", ease: "power5.out" });
       } else {
         // Closing actions
         gsap.to(".navbar--dropdown-content", { height: 0, ease: "power5.out" });
         gsap.to(siblingDropdownContent.find("a"), {
-          opacity: 0,
+          opacity: 0.5,
           ease: "power5.out",
         });
+        $(this).find(".navbar--dropdown-activestate").css("opacity", 0);
         gsap.to(".div-block-2", {
           height: 0,
           ease: "power5.out",
