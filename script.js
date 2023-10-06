9aefc1807f0c9ba30487e93b5b3fc8c13dbd5e1a
tarikjamil <tarikjamill@gmail.com>
2023-10-06 10:50:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -122,3 +122,35 @@ $(".contact--popup-bg").on("click", function () {
 $(".contact--button-close").on("click", function () {
   $(".popup--trigger").click();
 });
+
+// accordion --------------------- //
+$(".navbar--dropdown-trigger").on("click", function () {
+  if ($(window).width() < 998) {
+    // Only execute for screens below 998px
+
+    // Close other accordions when opening new one
+    if (!$(this).hasClass("open")) {
+      $(".navbar--dropdown-trigger.open").click();
+    }
+
+    // Save the sibling of the toggle we clicked on
+    let sibling = $(this).siblings(".navbar--dropdown-content");
+    let animationDuration = 500;
+
+    if ($(this).hasClass("open")) {
+      // Close the content div if already open
+      sibling.animate({ height: "0px" }, animationDuration);
+    } else {
+      // Open the content div if already closed
+      sibling.css("height", "auto");
+      let autoHeight = sibling.height();
+      sibling.css("height", "0px");
+      sibling.animate({ height: autoHeight }, animationDuration, () => {
+        sibling.css("height", "auto");
+      });
+    }
+
+    // Open and close the toggle div
+    $(this).toggleClass("open");
+  } // end of the screen width check
+});
