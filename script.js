1f88f5e87a32cb090d14325a488d338a9abf4a15
tarikjamil <tarikjamill@gmail.com>
2023-10-24 17:10:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -248,3 +248,31 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// accordion languages --------------------- //
+$(".lang--dropdown-trigger").on("click", function () {
+  // Close other accordions when opening new one
+  if (!$(this).hasClass("open")) {
+    $(".lang--dropdown-trigger.open").click();
+  }
+
+  // Save the sibling of the toggle we clicked on
+  let sibling = $(this).siblings(".lang--dropdown");
+  let animationDuration = 500;
+
+  if ($(this).hasClass("open")) {
+    // Close the content div if already open
+    sibling.animate({ height: "0px" }, animationDuration);
+  } else {
+    // Open the content div if already closed
+    sibling.css("height", "auto");
+    let autoHeight = sibling.height();
+    sibling.css("height", "0px");
+    sibling.animate({ height: autoHeight }, animationDuration, () => {
+      sibling.css("height", "auto");
+    });
+  }
+
+  // Open and close the toggle div
+  $(this).toggleClass("open");
+});
